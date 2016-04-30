c7269e36650942c6da9b4c7e00d81b81c3758c5d
Mariusz Nowak <medyk@medikoo.com>
2016-04-30 15:04:02 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# ag-sorted
+## Sort [ag](https://github.com/ggreer/the_silver_searcher) output by filenames alphabetically
+
+Temporary solution for lack of [--sort-files](https://github.com/ggreer/the_silver_searcher/issues/1) option
+
+## Tests [![Build Status](https://travis-ci.org/medikoo/ag-sorted.svg)](https://travis-ci.org/medikoo/ag-sorted)
+
+	$ npm test
---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var aFrom          = require('es5-ext/array/from')
+  , forEach        = require('es5-ext/object/for-each')
+  , ensureIterable = require('es5-ext/iterable/validate-object')
+  , deferred       = require('deferred')
+  , spawn          = require('child_process').spawn
+
+  , isColorArg = RegExp.prototype.test.bind(/^--(?:no)?color$/)
+  , isHeadingArg = RegExp.prototype.test.bind(/^-(?:H|-(?:no)?heading)$/);
+
+module.exports = function (args) {
+	var ag, def, map, currentName, currentData, init, cont;
+	args = (args == null) ? [] : aFrom(ensureIterable(args));
+
+	// Force default colors and headings (if not instructed otherwise)
+	// As we pipe data through this util, 'ag' detects its not a terminal
+	// and forces no colors and no headings. Adding below args reverts that behavior
+	if (!args.some(isColorArg)) args.push('--color');
+	if (!args.some(isHeadingArg)) args.push('-H');
+
+	ag = spawn('ag', args);
+	def = deferred();
+	map = {};
+
+	init = function (data) {
+		// Data for new file
+		currentData = [];
+		currentName = data.shift();
+		if (data.length) cont(data);
+	};
+	cont = function (data) {
+		// Continue gather of data
+		data.some(function (line, index) {
+			if (line) {
+				currentData.push(line);
+				return;
+			}
+			map[currentName] = currentData.join('\n');
+			currentData = null;
+			data = data.slice(index + 1);
+			if (data.length) init(data);
+			return true;
+		});
+	};
+
+	ag.stdout.on('data', function (data) {
+		data = String(data).split('\n');
+		if (!currentData) init(data);
+		else cont(data);
+	});
+
+	ag.stderr.on('data', function (data) { def.promise.emit('err', data); });
+
+	ag.on('error', def.reject);
+
+	ag.on('close', function () {
+		var result = [];
+		if (currentData) {
+			result[currentName] = currentData.join('\n');
+			currentData = null;
+		}
+		// Sort per filename
+		forEach(map, function (out, name) {
+			result.push(name + '\n' + out);
+		}, null, function (a, b) { return a.localeCompare(b); });
+		def.resolve(result.join('\n\n') + '\n');
+	});
+
+	return def.promise;
+};
